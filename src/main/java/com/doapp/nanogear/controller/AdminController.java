78e21772099c409c9a99af47609b11b425b143ad
HoangDuong2124 <103102495+HoangDuong2124@users.noreply.github.com>
2023-09-08 01:12:54 +0700

    update 1 so form va trang

--- src/main/java/com/doapp/nanogear/controller/AdminController.java
+++ src/main/java/com/doapp/nanogear/controller/AdminController.java
@@ -19,6 +19,19 @@ public class AdminController {
     public String AddProduct(){
         return "/admin/add_product";
     }
+    @GetMapping("/product_list")
+    public String ProductList(){
+        return "/admin/product_list";
+    }
+
+    @GetMapping("/add_category")
+    public String AddCategory(){
+        return "/admin/add_category";
+    }
+    @GetMapping("/category_list")
+    public String CategoryList(){
+        return "/admin/category_list";
+    }
 }
 
 
